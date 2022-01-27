1dfc73f64a1e2d96fa92c57fe2268fe9f172eb4c
art <art-nastya@inbox.ru>
2022-01-28 02:00:17 +0300

    create test

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,76 +1,61 @@
 import { Test } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
-import { Model, Query } from 'mongoose';
-import { createMock } from '@golevelup/nestjs-testing';
-
+import { Model} from 'mongoose';
 import { BooksService } from './books.service';
-import { Book, BookDocument } from './books.schema';
-
-const book = {
-    _id: '123456789',
-    title: 'title1',
-    description: 'description1',
-    authors: 'authors1',
-    favourite: true,
-    fileCover: 'fileCover1',
-    fileName: 'fileName1',
-    fileBook: 'fileBook1',
-};
-const service = {
-    getAll: jest.fn(),
-    getById: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(),
-};
+import {BookDocument, Books} from "./schemas/book.schema";
 
 describe('BooksService', () => {
     let booksService: BooksService;
     let bookModel: Model<BookDocument>;
+    const book = {
+        _id: '123456789',
+        title: 'title1',
+        description: 'description1',
+        authors: 'authors1',
+        favourite: true,
+        fileCover: 'fileCover1',
+        fileName: 'fileName1',
+        fileBook: 'fileBook1',
+    };
+    let model = {
+        findById: jest.fn(),
+        find: jest.fn(),
+        findByIdAndUpdate: jest.fn(),
+        deleteOne: jest.fn(),
+        create: jest.fn(),
+    };
 
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
             providers: [
                 BooksService,
                 {
-                    provide: getModelToken(Book.name),
-                    useValue: {
-                        find: jest.fn(),
-                        findById: jest.fn(),
-                        create: jest.fn(),
-                    },
+                    provide: getModelToken(Books.name),
+                    useValue: model,
                 },
             ],
         }).compile();
 
         booksService = moduleRef.get<BooksService>(BooksService);
-        bookModel = moduleRef.get<Model<BookDocument>>(getModelToken(Book.name));
+        bookModel = moduleRef.get<Model<BookDocument>>(getModelToken(Books.name));
     });
 
-    it('addBook', async () => {
-        jest.spyOn(bookModel, 'create').mockImplementationOnce(() => mockBook());
-        const result = await booksService.addBook(mockBook());
-
-        expect(result).toEqual(mockBook());
+    it('createBook', async () => {
+        jest.spyOn(bookModel, 'create').mockImplementation((book) => book);
+        const result = await booksService.createBook(book);
+        expect(result).toEqual(book);
     });
 
-    it('getAllBooks', async () => {
-        const booksArray = [mockBook(), mockBook()];
-
-        jest.spyOn(bookModel, 'find').mockResolvedValueOnce(booksArray as any);
-        const result = await booksService.getAllBooks();
-        expect(result).toEqual(booksArray);
+    it('getBooks', async () => {
+        const books = [book, book];
+        jest.spyOn(bookModel, 'find').mockImplementation(() => books as any);
+        const result = await booksService.getBooks();
+        expect(result).toEqual(books);
     });
 
-    it('getBookById', async () => {
-        jest.spyOn(bookModel, 'findById').mockReturnValueOnce(
-            createMock<Query<BookDocument, BookDocument>>({
-                exec: jest.fn().mockResolvedValueOnce(mockBook()),
-            }),
-        );
-
-        const result = await booksService.getBookById('id');
-
-        expect(result).toEqual(mockBook());
+    it('getBook', async () => {
+        jest.spyOn(bookModel, 'findById').mockReturnValueOnce(book as any);
+        const result = await booksService.getBook(book._id);
+        expect(result).toEqual(book);
     });
 });
